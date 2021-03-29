ab9b8bb0a59b68effb58f96ca3ce00534b4d0183
Lorena Spiller <lorenaspiller@gmail.com>
2021-03-29 17:51:32 +0200

    milestone 1 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+/*
+MILESTONE 1
+Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+dall’interlocutore (bianco) assegnando due classi CSS diverse
+● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
+nome e immagine di ogni contatto
+*/
+
 var app = new Vue({
   el: '#contacts',
   data: {
